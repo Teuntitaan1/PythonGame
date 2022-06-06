b44d724b8da43be08a6d36f042bfb3177220b078
Teuntitaan1 <Teun.weijdener@gmail.com>
2022-06-06 23:22:00 +0200

    made photo loading safer

--- Photos.py
+++ Photos.py
@@ -5,7 +5,9 @@ Photos = {}
 
 # Takes all the photos out of the Photos directory and puts them in a list
 for i in PhotosDir:
-    print("Found " + i + " in directory")
-    Photos[i] = pygame.image.load("Photos" + "\\" + i)
+    extension = os.path.splitext(i)
+    if extension[1] == '.png':
+        print("Found " + i + " in directory")
+        Photos[i] = pygame.image.load("Photos" + "\\" + i)
 
 print("loaded all photos")
